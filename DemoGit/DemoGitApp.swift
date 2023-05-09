d957fe71b1353c8462f510a6011780bed7d823d1
Maria Lopez <lopezmmatilde@hotmail.com>
2023-05-09 09:05:55 -0300

    Initial Commit

---
+++ DemoGit/DemoGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGitApp.swift
+//  DemoGit
+//
+//  Created by Maria Lopez on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
